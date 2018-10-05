82ba948e056564a07c2bcfcee6cf2f6101a375d4
ericrock461 <ericrock461@ed.amdsb.ca>
2018-10-05 13:46:36 -0400

    Add project files.

---
+++ cashRegister/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cashRegister
+{
+    public partial class Form1 : Form
+    {
+        /*tip: start EASY. Do the easiest things first, then start
+        your code off nice and slow, and expand on it
+        like Mr. T says, basics are most important, worry about 
+        aesthetics only if there's time*/
+
+        const double burgCost = 2.49("C");
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
